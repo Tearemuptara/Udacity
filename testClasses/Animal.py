c7e46855c406731bec78a125f28451c28ccfb132
Mark Beebe <mark.beebe@kroger.com>
2020-12-08 13:09:07 -0800

    Add some test classes to testClasses module

---
+++ testClasses/Animal.py
@@ -0,0 +1,48 @@
+class Animal:
+    home_planet = "Earth"
+
+    # Constructor (makes instances of the class)
+    def __init__(self,
+                 num_legs,
+                 num_arms,
+                 color,
+                 is_extinct,
+                 is_mythical):
+        self.num_legs = num_legs
+        self.num_arms = num_arms
+        self.color = color
+        self.total_limbs_val = num_arms + num_legs
+        self.is_extinct = is_extinct
+        self.is_mythical = is_mythical
+
+    # Alternate class constructor (makes instances of the class from strings)
+    @classmethod
+    def from_string(cls, string):
+        (num_legs, num_arms, color,
+         is_extinct, is_mythical) = string.split(" ")
+        num_legs = int(num_legs)
+        num_arms = int(num_arms)
+        is_extinct = bool(is_extinct)
+        is_mythical = bool(is_mythical)
+        return cls(num_legs, num_arms, color, is_extinct, is_mythical)
+
+    # Method that calculates total limbs
+    def total_limbs(self):
+        return self.num_legs + self.num_arms
+
+    # Class Method that sets home_planet Class Variable
+    @classmethod
+    def set_home_planet(cls, planet):
+        cls.home_planet = planet
+
+    # Static Method that prints a header
+    @staticmethod
+    def print_animals_header():
+        print("----Animals Are Cool----")
+
+
+horse = Animal(4, 0, "brown", False, False)
+unicorn = Animal(4, 0, "white", None, True)
+dog = Animal.from_string("4 0 black False False")
+
+Animal.print_animals_header()
---
+++ testClasses/Insect.py
@@ -0,0 +1,8 @@
+from testClasses.Animal import Animal
+
+
+class Insect(Animal):
+    pass
+
+
+dung_beetle = Insect(6, 0, "black", False, False)
