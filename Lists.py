13a393ca5468a00e1a69a9f81344bf9ad6943307
TearEmUpTara <taruhhdactyl@gmail.com>
2020-12-07 19:21:29 -0800

    Add Notes for Lesson 3.2 - Lists

---
+++ Lists.py
@@ -0,0 +1,23 @@
+list_of_random_things = [1, 3.4, 'a string', True]
+
+list_of_random_things[0]
+# Returns the first item in the list
+
+list_of_random_things[len(list_of_random_things)-1]
+# Returns the last thing
+# Don't forget to subtract ZEROth item >_>
+
+list_of_random_things[-1]
+# Last element
+# -2 is second to last, etc
+
+list_of_random_things = [1, 3.4, 'a string', True]
+list_of_random_things[1:2]
+# Returns 3.4 - skips 0th, grabs 1st, does not grab 2nd
+
+
+
+
+
+
+
