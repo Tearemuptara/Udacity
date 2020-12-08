337801f6622f97c6642b21d33361fa36878146b5
Mark Beebe <mark.beebe@kroger.com>
2020-12-07 19:25:32 -0800

    Make conflicting changes to Lists.py

--- Lists.py
+++ Lists.py
@@ -3,7 +3,7 @@ list_of_random_things = [1, 3.4, 'a string', True]
 list_of_random_things[0]
 # Returns the first item in the list
 
-list_of_random_things[len(list_of_random_things)-1]
+list_of_random_things[len(list_of_random_things) - 1]
 # Returns the last thing
 # Don't forget to subtract ZEROth item >_>
 
@@ -16,8 +16,19 @@ list_of_random_things[1:2]
 # Returns 3.4 - skips 0th, grabs 1st, does not grab 2nd
 
 
+# Here's a change to mess up Tara's stuff
+
+# Mess mess mess, mess mess mess
+# So many conflicts
+
+print("Mark is the coolest")
 
 
 
 
 
+
+
+
+
+# this is the end of the file
